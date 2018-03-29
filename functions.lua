96865bdfeb94a9f6d71aa4df2b72fb21021b724a
Finn <36964028+FinnRules@users.noreply.github.com>
2018-03-29 12:05:47 -0400

    Update functions.lua
    incomplete

--- functions.lua
+++ functions.lua
@@ -1,1 +1,7 @@
 --where the building functions will be stored
+
+--basic pillar | F
+function pillar (loc, height, xoffset, yoffset, zoffset, node) --node is the full itemname as a string
+  minetest.set_node({x = pos.x + xoffset, y = pos.y + yoffset, z = pos.z + zoffset}, {name = node})
+  
+end,
