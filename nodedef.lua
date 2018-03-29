d184012c538398a04de5648f9e563851135f8c1a
Finn <36964028+FinnRules@users.noreply.github.com>
2018-03-29 11:47:33 -0400

    Update nodedef.lua

--- nodedef.lua
+++ nodedef.lua
@@ -1,1 +1,6 @@
 --new nodes go in here
+minetest.register_node("instahouse:house_small", {
+		tiles = {"instahouse_house_small.png"},
+		groups = {oddly_breakable_by_hand=2},
+		description = "Small House"
+})
