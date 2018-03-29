49a2a09c1e724f283930ef670cf8aed4329a77e6
Finn <36964028+FinnRules@users.noreply.github.com>
2018-03-29 15:07:43 -0400

    Update nodedef.lua
    version using function abstraction

--- nodedef.lua
+++ nodedef.lua
@@ -4,7 +4,13 @@ minetest.register_node("instahouse:house_small", {
 	groups = {oddly_breakable_by_hand=2},
 	description = "Small House",
 	after_place_node = func(pos)
-		wall(-3,0,0,"default:stonebrick","x"),
+		smallhouse(o, o, o, "default:stonebrick", "default:tree", "default:cobble", "default:cobble"),
+	end,
+})
+
+
+--[[
+wall(-3,0,0,"default:stonebrick","x"),
 		wall(3,0,0,"default:stonebrick","x"),
 		wall(0,0,-3,"default:stonebrick","z"),
 		wall(0,0,3,"default:stonebrick","z"),
@@ -12,5 +18,5 @@ minetest.register_node("instahouse:house_small", {
 		pillar(4, 3, yoffset, 3, "default:tree"),
 		pillar(4, -3, yoffset, -3, "default:tree"),
 		pillar(4, 3, yoffset, -3, "default:tree")
-	end,
-})
+
+]]--
