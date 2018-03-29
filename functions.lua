0e1a7e3c3b7ba5028aa4f006dd70c2966c286267
Finn <36964028+FinnRules@users.noreply.github.com>
2018-03-29 12:43:23 -0400

    Update functions.lua

--- functions.lua
+++ functions.lua
@@ -2,9 +2,14 @@
 
 --basic pillar | F
 function pillar (loc, height, xoffset, yoffset, zoffset, node) --node is the full itemname as a string
-  minetest.set_node({x = pos.x + xoffset, y = pos.y + yoffset, z = pos.z + zoffset}, {name = node})
-  
+  local tall = 0;
+  for tall < height do
+    minetest.set_node({x = pos.x + xoffset, y = pos.y + yoffset + tall, z = pos.z + zoffset}, {name = node}),
+    height =  height + 1,
+  end,
 end,
+
+--example call | pillar(pos, 4, 0, 0, 0, "default:dirt")
 --builds a wall | A
 function wall (loc, height, xoffset, yoffset, zoffset, node)
   minetest.set_node(
