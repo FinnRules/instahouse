cd4af692f785e71d9fd403a9c78bdd30dc5f7958
Finn <36964028+FinnRules@users.noreply.github.com>
2018-03-30 10:16:55 -0400

    Create crafting.lua

---
+++ crafting.lua
@@ -0,0 +1,8 @@
+minetest.register_craft({
+		output = 'instahouse:small_house',
+		recipe = {
+				{'default:wood', 'default:wood', 'default:wood'},
+				{'default:cobble', 'default:cobble', 'default:cobble'},
+				{'default:cobble', 'default:cobble', 'default:cobble'},
+		}
+})
