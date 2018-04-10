f1d566412de6b61f4f4e1b59d3ae869e1bf63b3c
Finn <36964028+FinnRules@users.noreply.github.com>
2018-04-10 11:57:32 -0400

    Update crafting.lua
    added crafting for everything. Theoretically, the mod is now finished

--- crafting.lua
+++ crafting.lua
@@ -1,8 +1,39 @@
+--Small house crafting
 minetest.register_craft({
 		output = 'instahouse:small_house',
 		recipe = {
 				{'default:wood', 'default:wood', 'default:wood'},
+				{'default:cobble', 'instahouse:gigantomite', 'default:cobble'},
 				{'default:cobble', 'default:cobble', 'default:cobble'},
-				{'default:cobble', 'default:cobble', 'default:cobble'},
+		}
+})
+
+--Mansion crafting
+minetest.register_craft({
+		output = 'instahouse:mansion',
+		recipe = {
+				{'instahouse:small_house', 'instahouse:small_house', 'instahouse:small_house'},
+				{'instahouse:small_house', 'instahouse:gigantomite', 'instahouse:small_house'},
+				{'instahouse:small_house', 'instahouse:small_house', 'instahouse:small_house'},
+		}
+})
+
+--Super mansion crafting
+minetest.register_craft({
+		output = 'instahouse:super_mansion',
+		recipe = {
+				{'instahouse:mansion', 'instahouse:mansion', 'instahouse:mansion'},
+				{'instahouse:mansion', 'instahouse:gigantomite', 'instahouse:mansion'},
+				{'instahouse:mansion', 'instahouse:mansion', 'instahouse:mansion'},
+		}
+})
+
+--Skyscraper Crafting
+minetest.register_craft({
+		output = 'instahouse:skyscraper',
+		recipe = {
+				{'default:steelblock', 'instahouse:gigantomite', 'default:steelblock'},
+				{'default:glass', 'instahouse:gigantomite', 'default:glass'},
+				{'default:steelblock', 'instahouse:gigantomite', 'default:steelblock'},
 		}
 })
