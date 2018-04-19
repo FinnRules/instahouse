9f69d94accf093d9bfe56cb55a20e1822a7477d3
Finn <36964028+FinnRules@users.noreply.github.com>
2018-04-19 16:36:19 -0400

    Update init.lua
    added comments

--- init.lua
+++ init.lua
@@ -9,6 +9,6 @@ _/     _/ _/  _/ _/  _/  _/  _/ _/_/_/ _/ _/_/_/ _/_/_/
 instahouse = {}
 instahouse.path = minetest.get_modpath("instahouse")
 
-dofile(instahouse.path .. "/functions.lua")
-dofile(instahouse.path .. "/nodedef.lua")
-dofile(instahouse.path .. "/crafting.lua")
+dofile(instahouse.path .. "/functions.lua") --creates functions
+dofile(instahouse.path .. "/nodedef.lua")   --defines nodes with function calls
+dofile(instahouse.path .. "/crafting.lua")  --facilitates ingame crafting
