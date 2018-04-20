3f3575638341af66ca560269c4772e540666db54
Finn <36964028+FinnRules@users.noreply.github.com>
2018-04-20 05:12:43 -0400

    Update init.lua
    added notes

--- init.lua
+++ init.lua
@@ -6,8 +6,8 @@
 _/     _/ _/  _/ _/  _/  _/  _/ _/_/_/ _/ _/_/_/ _/_/_/
 ]]--
 
-instahouse = {}
-instahouse.path = minetest.get_modpath("instahouse")
+instahouse = {} 
+instahouse.path = minetest.get_modpath("instahouse") --grabs the path to the mod folder on your computer
 
 dofile(instahouse.path .. "/functions.lua") --creates functions
 dofile(instahouse.path .. "/nodedef.lua")   --defines nodes with function calls
